6f649ae6a0f6936b94e76c178ebda38d407b868a
djkaz <justbekazu@gmail.com>
2021-07-04 18:16:28 -0500

    amended index

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const http = require("http");
 const util = require("util");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const Employee = require("./lib/Employee");
 const generateHTML = require("./src/generateHTML");
 
-// writeFile function
 const writeFileAsync = util.promisify(fs.writeFile);
 
-// Team array [[manager], [engineer/s], [intern/s]]
 const team = [[], [], []];
 
 // Runs next function according to inquirer prompt menu selection
@@ -21,103 +17,100 @@ function nextPrompt() {
     case "Add an" + chalk.bold(" ENGINEER") + " to my team":
       engineerInfo();
       break;
-    case "Add an" + chalk.bold(" INTERN") + " to my team":
+    case "Add an" + " INTERN" + " to my team":
       internInfo();
       break;
-    case "Add another" + chalk.bold(" ENGINEER") + " to my team":
+    case "Add another" + " ENGINEER" + " to my team":
       engineerInfo();
       break;
-    case "Add another" + chalk.bold(" INTERN") + " to my team":
+    case "Add another" + " INTERN" + " to my team":
       internInfo();
-      break;
+    //break;
     default:
-      writeFileAsync("./dist/profile.html", generateHTML(team))
-        .then(() =>
-          console.log(
-            chalk.black.bgGreenBright(" New HTML file successfully generated! ")
-          )
-        )
-        .catch((err) =>
-          console.log(
-            chalk.black.bgRedBright(" Oops, there was an error... ", err)
-          )
-        );
-      break;
+      writeFileAsync("./dist/profile.html", generateHTML(team)).then(() =>
+        console
+          .log(" New HTML file successfully generated! ")
+
+          .catch((err) => console.log(" Oops, there was an error... ", err))
+      );
   }
 }
 
-// Prompt validation functions
+// function validateFirstName(firstName) {
+//   if (name.length <= 20 && name.length > 0) {
+//     return true;
+//   }
+//   return ("Must be between 1-20 characters (inclusive)");
+// }
 function validateName(name) {
-  if (name.length <= 25 && name.length > 0) {
+  if (name.length <= 20 && name.length > 0) {
     return true;
   }
-  return chalk.redBright("Name must be between 1-25 characters (inclusive)");
+  return "Must be between 1-20 characters (inclusive)";
 }
 
-function validateNumber(num) {
-  if (/^[0-9]+$/.test(num)) {
+function validateNumber(number) {
+  if (/^[0-9]+$/.test(number)) {
     return true;
   }
-  return chalk.redBright("Only numeric values are accepted");
+  return "numbers only";
 }
 
 function validateEmail(email) {
   if (/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(email)) {
     return true;
   }
-  return chalk.redBright("Please input a valid email address");
+  return "Please input a valid email address";
 }
 
 function validateGithub(github) {
   if (/^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/i.test(github)) {
     return true;
   }
-  return chalk.redBright("Please input a valid GitHub username");
+  return "Please input a valid GitHub username";
 }
 
-// Manager prompts
 const managerInfo = () => {
   inquirer
     .prompt([
       {
         type: "input",
         name: "managerName",
-        message: "Team manager " + chalk.italic("name") + " (1-25 characters):",
+        message: "Team manager " + "name" + " (1-25 characters):",
         validate: validateName,
       },
       {
         type: "input",
         name: "managerId",
-        message: "Team manager " + chalk.italic("employee ID") + ":",
+        message: "Team manager " + "employee ID" + ":",
         validate: validateNumber,
       },
       {
         type: "input",
         name: "managerEmail",
-        message: "Team manager " + chalk.italic("email") + ":",
+        message: "Team manager " + "email" + ":",
         validate: validateEmail,
       },
       {
         type: "input",
         name: "managerOfficeNumber",
-        message: "Team manager " + chalk.italic("office number") + ":",
+        message: "Team manager " + "office number" + ":",
         validate: validateNumber,
       },
       {
         type: "list",
         name: "menu",
-        message: chalk.yellow(
-          "--------- Menu --------- \nChoose one of the following options:"
-        ),
+        message:
+          "--------- Menu --------- \nChoose one of the following options:",
         choices: [
           {
-            name: "Add an" + chalk.bold(" ENGINEER") + " to my team",
+            name: "Add an" + " ENGINEER" + " to my team",
           },
           {
-            name: "Add an" + chalk.bold(" INTERN") + " to my team",
+            name: "Add an" + " INTERN" + " to my team",
           },
           {
-            name: "My team is complete",
+            name: "Finished",
           },
         ],
       },
@@ -135,46 +128,44 @@ const managerInfo = () => {
     });
 };
 
-// Engineer prompts
 const engineerInfo = () => {
   inquirer
     .prompt([
       {
         type: "input",
         name: "engineerName",
-        message: "Engineer" + chalk.italic(" name") + " (1-25 characters):",
+        message: "Engineer" + " name" + " (1-25 characters):",
         validate: validateName,
       },
       {
         type: "input",
         name: "engineerId",
-        message: "Engineer" + chalk.italic(" employee ID") + ":",
+        message: "Engineer" + " employee ID" + ":",
         validate: validateNumber,
       },
       {
         type: "input",
         name: "engineerEmail",
-        message: "Engineer" + chalk.italic(" email") + ":",
+        message: "Engineer" + " email" + ":",
         validate: validateEmail,
       },
       {
         type: "input",
         name: "engineerGithub",
-        message: "Engineer" + chalk.italic(" GitHub username") + ":",
+        message: "Engineer" + " GitHub username" + ":",
         validate: validateGithub,
       },
       {
         type: "list",
         name: "menu",
-        message: chalk.yellow(
-          "--------- Menu --------- \nChoose one of the following options:"
-        ),
+        message:
+          "--------- Menu --------- \nChoose one of the following options:",
         choices: [
           {
-            name: "Add another" + chalk.bold(" ENGINEER") + " to my team",
+            name: "Add another" + " ENGINEER" + " to my team",
           },
           {
-            name: "Add an" + chalk.bold(" INTERN") + " to my team",
+            name: "Add an" + " INTERN" + " to my team",
           },
           {
             name: "My team is complete",
@@ -195,45 +186,43 @@ const engineerInfo = () => {
     });
 };
 
-// Intern prompts
 const internInfo = () => {
   inquirer
     .prompt([
       {
         type: "input",
         name: "internName",
-        message: "Intern" + chalk.italic(" name") + " (1-25 characters):",
+        message: "Intern" + " name" + " (1-25 characters):",
         validate: validateName,
       },
       {
         type: "input",
         name: "internId",
-        message: "Intern" + chalk.italic(" employee ID") + ":",
+        message: "Intern" + " employee ID" + ":",
         validate: validateNumber,
       },
       {
         type: "input",
         name: "internEmail",
-        message: "Intern" + chalk.italic(" email") + ":",
+        message: "Intern" + " email" + ":",
         validate: validateEmail,
       },
       {
         type: "input",
         name: "internSchool",
-        message: "Intern" + chalk.italic(" school") + ":",
+        message: "Intern" + " school" + ":",
       },
       {
         type: "list",
         name: "menu",
-        message: chalk.yellow(
-          "--------- Menu --------- \nChoose one of the following options:"
-        ),
+        message:
+          "--------- Menu --------- \nChoose one of the following options:",
         choices: [
           {
-            name: "Add an" + chalk.bold(" ENGINEER") + " to my team",
+            name: "Add an" + " ENGINEER" + " to my team",
           },
           {
-            name: "Add another" + chalk.bold(" INTERN") + " to my team",
+            name: "Add another" + " INTERN" + " to my team",
           },
           {
             name: "My team is complete",
